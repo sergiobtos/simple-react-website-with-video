bd8ae560bd0b6e6be28a6c8f48a4ee48c6212b70
Sergio Silva <sergiobtos@hotmail.com>
2021-06-02 00:24:34 -0400

    Footer component added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import  { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -10,6 +13,29 @@ function App() {
       <Navbar/>
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route path='/services' exact component={Services} />
+        <Route path='/products' exact component={Products} />
+        <Route path='/sign-up' exact component={SignUp} />
+        <Route path='/facebook' component={() => {
+          window.location.href = "https://www.facebook.com/";
+          return null;
+        }}/>
+        <Route path='/instagram' component={() => {
+          window.location.href = "https://www.instagram.com/";
+          return null;
+        }}/>
+        <Route path='/youtube' component={() => {
+          window.location.href = "https://www.youtube.com/";
+          return null;
+        }}/>
+        <Route path='/twitter' component={() => {
+          window.location.href = "https://twitter.com/home";
+          return null;
+        }}/>
+        <Route path='/linkedin' component={() => {
+          window.location.href = "https://www.linkedin.com/feed/";
+          return null;
+        }}/>
       </Switch>
     </Router>
     </>
